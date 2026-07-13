61f0191e0e4b9ad6b078a817ccb16b776980d1da
MarlonEcr <e1751059815@uisrael.edu.ec>
2026-07-12 23:23:23 -0500

    feat: estructura inicial proyecto SIAV_WEB-PISIP con plantilla PlainAdmin

---
+++ src/main/java/com/siav_pisip/siav_web/SiavWebApplication.java
@@ -0,0 +1,13 @@
+package com.siav_pisip.siav_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiavWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiavWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siav_pisip/siav_web/SiavWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siav_pisip.siav_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiavWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
